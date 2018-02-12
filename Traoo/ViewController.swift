76e097a46bc3b9edb9a9be51484f500c96567d22
PoYee Tam <poyee@PoYees-MacBook-Air.local>
2018-02-12 16:21:38 +0800

    Initial Commit

---
+++ Traoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Traoo
+//
+//  Created by PoYee Tam on 12/2/2018.
+//  Copyright © 2018年 PoYee Tam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
