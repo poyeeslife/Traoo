c23b9508d7671e02574eb44a4b67570133988bdf
PoYee Tam <poyee@PoYees-MacBook-Air.local>
2018-02-16 21:07:58 +0800

    Data persisted using user default method

--- Traoo/TableViewController.swift
+++ Traoo/TableViewController.swift
@@ -12,10 +12,15 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+        
+            itemArray = items}
+        // if statement becoz if theres no saved data on todolistarray, app will crashed.
     }
 
  var itemArray = ["do first thing" , "do second", "do third"]
+    let defaults = UserDefaults.standard
     // var becoz we need to add something..
     
     //MARK - Tableview Datasource Method
@@ -65,6 +70,8 @@ class TableViewController: UITableViewController {
             // what will happen once the user clicks the add item button on our UIAlert
             print(textfield.text)
             self.itemArray.append(textfield.text!)
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            //key is use to identify the array in user defaults
             self.tableView.reloadData()
         }
         
@@ -74,4 +81,9 @@ class TableViewController: UITableViewController {
     }
     
 }
+
+// use the saved items to load up our tableview to load up all the data
+// we can view our data inside our user default and we can prove that the data has been saved
+//user def gets saved in a plist file.. thats why everything we saved need to be a key-value pair.. we need a key (i.e. "ToDoListArray" to retrieve the item and then add a value of any type (e.g. string/dictionary/array)
+// need to run in viewDiDLoad
 
